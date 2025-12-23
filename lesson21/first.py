69030e753dd4dbe30752e045a5f91318cb1ebe73
alanting <alanting0701@gmail.com>
2025-12-23 21:33:06 +0800

    modify

--- lesson21/first.py
+++ lesson21/first.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1 style="color:red">Hello, World! 歡迎使用 Flask!</h1>'
+    return '<h1 style="color:Blue">上傳成功! 歡迎使用 Flask!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
